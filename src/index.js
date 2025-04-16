c2b339e025dc3f97d1ab2f6715d494c888a718fc
nata20019 <nbernikova68@gmail.com>
2025-04-16 10:43:46 +0200

    mod113333

--- src/index.js
+++ src/index.js
@@ -1,93 +1,79 @@
 import axios from 'axios';
 import Notiflix, { Notify } from 'notiflix';
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const PIXABAY_KEY = '49707208-ae8e491fbb4cb60d419a5399f';
 const BASE_URL = 'https://pixabay.com/api/';
+
 const form = document.querySelector('.search-form');
 const input = document.querySelector('input');
 const button = document.querySelector('button');
+const galleryList = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.load-more');
 
-form.addEventListener('submit', onSelect);
+form.addEventListener('submit', onSearch);
+loadMoreBtn.addEventListener('click', fetchImages);
 
-function onSelect(evt) {
+function onSearch(evt) {
   evt.preventDefault();
-  console.log(evt.target);
-  const currentIndexOption = evt.target.id;
-  //   const idImage = evt.target.[currentIndexOption].value;
-  //   console.log(idImage);
-  //   catInfo.innerHTML = '';
-
-  fetchImages(idImage).then(data => {
-    if (data.hits.length === 0) {
+  const searchQuery = evt.currentTarget.elements.searchQuery.value.trim();
+  console.log(searchQuery);
+  fetchImages(searchQuery).then(data => {
+    console.log(data);
+    if (data.data.hits.length === 0) {
       Notify.failure('Error! Please, try again.');
-
       return;
     }
-    console.log(data.hits[0].user);
-    // console.log(data);
-    //      const image = {
-    //   webformatURL,
-    //   largeImageURL,
-    //   tags,
-    //   likes,
-    //   views,
-    //   comments,
-    //   downloads,
-    // }; = data.data[0].image;
-    //     const { url } = data.data[0];
-    cardInfo.innerHTML = createMarkup(
-      webformatURL,
-      largeImageURL,
-      tags,
-      likes,
-      views,
-      comments,
-      downloads
-    );
+    galleryList.innerHTML = createMarkup(data.data.hits);
   });
 }
-// const image = {
-//   webformatURL,
-//   largeImageURL,
-//   tags,
-//   likes,
-//   views,
-//   comments,
-//   downloads,
-// };
-function fetchImages() {
+
+function fetchImages(query) {
   return axios(
-    `${BASE_URL}?key=${PIXABAY_KEY}&q=yellow+flower&image_type=photo&orientation=horizontal&safesearch=true&page=1&per_page=20`
-  )
-    .then(r => r.json())
-    .then(console.log(r));
+    `${BASE_URL}?key=${PIXABAY_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=1&per_page=40`
+  );
 }
 fetchImages();
 
-function createMarkup(
-  webformatURL,
-  largeImageURL,
-  Tags,
-  Likes,
-  Views,
-  Comments,
-  Downloads
-) {
-  return `<div class="photo-card">
-  <img src="${webformatURL}" alt="${Tags}" loading="lazy" />
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `<div class="photo-card">
+      <a href="${largeImageURL}">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" width="300" height="200"/>
   <div class="info">
     <p class="info-item">
-      <b>${Likes}</b>
+      <b>Likes</b>${likes}
     </p>
     <p class="info-item">
-      <b>${Views}</b>
+      <b>Views</b>${views}
     </p>
     <p class="info-item">
-      <b>${Comments}</b>
+      <b>Comments</b>${comments}
     </p>
     <p class="info-item">
-      <b>${Downloads}</b>
+      <b>Downloads</b>${downloads}
     </p>
-  </div>
-</div>`;
+     </div>
+     </a>
+</div>`
+    )
+    .join('');
 }
+
+// galleryList.insertAdjacentHTML('beforeend', markupItems);
+// const lightbox = new SimpleLightbox('.gallery a', {
+//   captionsData: 'alt',
+//   captionDelay: 250,
+// });
