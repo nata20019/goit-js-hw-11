1aa903cda76caf5d55e1ae8bfb2ed020dc10997a
nata20019 <nbernikova68@gmail.com>
2025-04-16 12:56:39 +0200

    mod111111

--- src/index.js
+++ src/index.js
@@ -1,39 +1,166 @@
 import axios from 'axios';
-import Notiflix, { Notify } from 'notiflix';
+import Notiflix, { Notify, Loading } from 'notiflix';
+// import { Loading } from 'notiflix/build/notiflix-loading-aio';
+
 // Описаний в документації
 import SimpleLightbox from 'simplelightbox';
 // Додатковий імпорт стилів
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
 const PIXABAY_KEY = '49707208-ae8e491fbb4cb60d419a5399f';
 const BASE_URL = 'https://pixabay.com/api/';
 
 const form = document.querySelector('.search-form');
-const input = document.querySelector('input');
-const button = document.querySelector('button');
 const galleryList = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more');
+let page = 1;
+let searchQuery = '';
 
 form.addEventListener('submit', onSearch);
-loadMoreBtn.addEventListener('click', fetchImages);
+loadMoreBtn.addEventListener('click', onLoadMore);
 
-function onSearch(evt) {
+async function onSearch(evt) {
   evt.preventDefault();
-  const searchQuery = evt.currentTarget.elements.searchQuery.value.trim();
+  searchQuery = evt.currentTarget.elements.searchQuery.value.trim();
   console.log(searchQuery);
-  fetchImages(searchQuery).then(data => {
+  if (!searchQuery) {
+    Notify.failure('Error! Please, type somerthing into input.');
+    return;
+  }
+  Loading.dots();
+  page = 1;
+  // fetchImages(searchQuery, page)
+  //   .then(data => {
+  //     console.log(data);
+  //     if (data.data.hits.length === 0) {
+  //       Notify.failure(
+  //         'Sorry, there are no images matching your search query. Please try again.'
+  //       );
+  //       galleryList.innerHTML = '';
+  //       loadMoreBtn.setAttribute('hidden', true);
+  //       return;
+  //     }
+  //     Loading.remove();
+
+  //     Notify.info(`Hooray! We found ${data.data.totalHits} images.`);
+  //     galleryList.innerHTML = createMarkup(data.data.hits);
+  //     lightbox.refresh();
+  //     if (page * 40 < data.data.totalHits) {
+  //       loadMoreBtn.removeAttribute('hidden');
+  //     } else {
+  //       loadMoreBtn.setAttribute('hidden', true);
+  //       Notify.info(
+  //         "We're sorry, but you've reached the end of search results."
+  //       );
+  //     }
+  //   })
+  //   .catch(e => {
+  //     Notify.failure(`Something wrong ${e.message}`);
+  //     galleryList.innerHTML = '';
+  //     loadMoreBtn.setAttribute('hidden', true);
+  //   })
+  //   .finally(() => {
+  //     Loading.remove();
+  //   });
+  try {
+    const data = await fetchImages(searchQuery, page);
     console.log(data);
     if (data.data.hits.length === 0) {
-      Notify.failure('Error! Please, try again.');
+      Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      galleryList.innerHTML = '';
+      loadMoreBtn.setAttribute('hidden', true);
       return;
     }
+    Loading.remove();
+
+    Notify.info(`Hooray! We found ${data.data.totalHits} images.`);
     galleryList.innerHTML = createMarkup(data.data.hits);
-  });
+    lightbox.refresh();
+    if (page * 40 < data.data.totalHits) {
+      loadMoreBtn.removeAttribute('hidden');
+    } else {
+      loadMoreBtn.setAttribute('hidden', true);
+      Notify.info("We're sorry, but you've reached the end of search results.");
+    }
+  } catch (error) {
+    Notify.failure(`Something wrong ${e.message}`);
+    galleryList.innerHTML = '';
+    loadMoreBtn.setAttribute('hidden', true);
+  } finally {
+    Loading.remove();
+  }
 }
 
-function fetchImages(query) {
+async function onLoadMore() {
+  page += 1;
+  Loading.dots();
+
+  // fetchImages(searchQuery, page)
+  //   .then(data => {
+  //     console.log(data);
+  //     Loading.remove();
+  //     galleryList.insertAdjacentHTML('beforeend', createMarkup(data.data.hits));
+  //     lightbox.refresh();
+  //     const { height: cardHeight } = document
+  //       .querySelector('.gallery')
+  //       .firstElementChild.getBoundingClientRect();
+
+  //     window.scrollBy({
+  //       top: cardHeight * 2,
+  //       behavior: 'smooth',
+  //     });
+  //     if (page * 40 < data.data.totalHits) {
+  //       loadMoreBtn.removeAttribute('hidden');
+  //     } else {
+  //       loadMoreBtn.setAttribute('hidden', true);
+  //       Notify.info(
+  //         "We're sorry, but you've reached the end of search results."
+  //       );
+  //     }
+  //   })
+  //   .catch(e => {
+  //     Notify.failure(`Something wrong ${e.message}`);
+  //   })
+  //   .finally(() => {
+  //     Loading.remove();
+  //   });
+  try {
+    const data = await fetchImages(searchQuery, page);
+    console.log(data);
+    Loading.remove();
+    galleryList.insertAdjacentHTML('beforeend', createMarkup(data.data.hits));
+    lightbox.refresh();
+    const { height: cardHeight } = document
+      .querySelector('.gallery')
+      .firstElementChild.getBoundingClientRect();
+
+    window.scrollBy({
+      top: cardHeight * 2,
+      behavior: 'smooth',
+    });
+    if (page * 40 < data.data.totalHits) {
+      loadMoreBtn.removeAttribute('hidden');
+    } else {
+      loadMoreBtn.setAttribute('hidden', true);
+      Notify.info("We're sorry, but you've reached the end of search results.");
+    }
+  } catch (error) {
+    Notify.failure(`Something wrong ${e.message}`);
+  } finally {
+    Loading.remove();
+  }
+}
+
+function fetchImages(query, page = 1) {
   return axios(
-    `${BASE_URL}?key=${PIXABAY_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=1&per_page=40`
+    `${BASE_URL}?key=${PIXABAY_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`
   );
 }
 fetchImages();
@@ -52,6 +179,7 @@ function createMarkup(arr) {
       }) => `<div class="photo-card">
       <a href="${largeImageURL}">
   <img src="${webformatURL}" alt="${tags}" loading="lazy" width="300" height="200"/>
+  </a>
   <div class="info">
     <p class="info-item">
       <b>Likes</b>${likes}
@@ -66,14 +194,8 @@ function createMarkup(arr) {
       <b>Downloads</b>${downloads}
     </p>
      </div>
-     </a>
+  
 </div>`
     )
     .join('');
 }
-
-// galleryList.insertAdjacentHTML('beforeend', markupItems);
-// const lightbox = new SimpleLightbox('.gallery a', {
-//   captionsData: 'alt',
-//   captionDelay: 250,
-// });
